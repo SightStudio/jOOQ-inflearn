fe5e804598d0f65c0441b7e9c1268a04fa6968f9
SightStudio <sdm3285@gmail.com>
2024-03-06 22:35:07 +0900

    jOOQ gradle + testContainer + flyway 세팅

---
+++ gradle-testcontainer-flyway/src/main/java/org/sight/gradletestcontainerflyway/GradleTestContainersFlywayApplication.java
@@ -0,0 +1,13 @@
+package org.sight.gradletestcontainerflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestContainersFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleTestContainersFlywayApplication.class, args);
+    }
+
+}
---
+++ gradle-testcontainer-flyway/src/test/java/org/sight/gradletestcontainerflyway/GradleTestcontainerFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sight.gradletestcontainerflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleTestcontainerFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
