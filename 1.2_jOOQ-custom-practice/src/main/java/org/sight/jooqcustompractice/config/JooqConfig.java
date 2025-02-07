fbff829a697fe5a4f32ec5ea226d3afbdf0d585a
Jungmini <jungmini0601@gmail.com>
2025-02-07 15:07:56 +0900

    Update JooqConfig.java
    Jooq Config가 SpringBoot2.0대로 적용되어 있는 내용 수정

--- 1.2_jOOQ-custom-practice/src/main/java/org/sight/jooqcustompractice/config/JooqConfig.java
+++ 1.2_jOOQ-custom-practice/src/main/java/org/sight/jooqcustompractice/config/JooqConfig.java
@@ -1,15 +1,15 @@
 package org.sight.jooqcustompractice.config;
 
-import org.jooq.conf.Settings;
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JooqConfig {
 
-    @Bean
-    Settings jooqSettings() {
-        return new Settings()
-                .withRenderSchema(false);
-    }
+	@Bean
+	public DefaultConfigurationCustomizer jooqDefaultConfigurationCustomizer() {
+		return c -> c.settings()
+			.withRenderSchema(false);
+	}
 }
